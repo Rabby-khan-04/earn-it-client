f2e7608a08bb5cfc7f60b408960f22a29b056a55
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-02-24 16:37:25 +0600

    Setup earn it project

---
+++ src/routes/router.jsx
@@ -0,0 +1,18 @@
+import Main from "@/layouts/Main";
+import Home from "@/pages/Home/Home";
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
