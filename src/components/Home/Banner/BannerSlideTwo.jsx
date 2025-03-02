04441eb92c5028ec332e3217a28114f568341795
Rabby-khan-04 <ajrabbyk72@gmail.com>
2025-03-02 15:56:25 +0600

    added another banner slide

--- src/components/Home/Banner/BannerSlideTwo.jsx
+++ src/components/Home/Banner/BannerSlideTwo.jsx
@@ -1,5 +1,56 @@
+import bannerBg from "@/assets/images/banner/banner-bg-2.jpg";
+import bannerImg from "@/assets/images/banner/banner-img-2.png";
+import BannerText from "./BannerText";
+import { Link } from "react-router-dom";
+import BaseIconStats from "./BaseIconStats";
+import { AiFillLike } from "react-icons/ai";
+
 const BannerSlideTwo = () => {
-  return <div>BnnerSlideTwo</div>;
+  return (
+    <div
+      className="h-[800px] pt-28 pb-14 bg-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: `url(${bannerBg})` }}
+    >
+      <div className="container grid lg:grid-cols-2 gap-8">
+        <div className="self-center">
+          <BannerText
+            subtitle="Instant Hiring"
+            title="Thousands of Verified Experts Ready to Work for You"
+            text="From micro-jobs to large projects, find skilled professionals and get your work done efficiently."
+            badge="#21dfd63e"
+          />
+          <div className="mt-5">
+            <Link className="btn__primary" to="/auth/login">
+              Hire Top Talent
+            </Link>
+          </div>
+        </div>
+        <div className="justify-self-end relative">
+          <img src={bannerImg} className="w-[650px] inline-block" alt="" />
+          <div className="absolute left-0 top-3/4">
+            <BaseIconStats
+              ICON={AiFillLike}
+              title="Ariene McCoy"
+              subtitle="UX/UI Designer"
+              count="4.9"
+              color="#168882"
+              cssClass="rounded-3xl rounded-tr-none"
+            />
+          </div>
+          <div className="absolute -right-20 top-1/2">
+            <BaseIconStats
+              ICON={AiFillLike}
+              title="Jane Cooper"
+              subtitle="UX/UI Designer"
+              count="4.9"
+              color="#f6cc77"
+              cssClass="rounded-3xl rounded-tl-none"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BannerSlideTwo;
---
+++ src/components/Home/Banner/BaseIconStats.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import { IoStar } from "react-icons/io5";
+
+const BaseIconStats = ({ ICON, title, subtitle, count, cssClass, color }) => {
+  return (
+    <div className={`bg-white min-w-[300px] inline-block ${cssClass} p-6`}>
+      <div className="flex items-center gap-3">
+        <div
+          className="size-14 rounded-full flex items-center justify-center"
+          style={{ backgroundColor: color }}
+        >
+          <ICON className="text-2xl text-white" />
+        </div>
+        <div>
+          <h3 className="font-dm-sans text-title text-xl font-semibold">
+            {title}
+          </h3>
+          <p className="text-sm font-inter font-medium text-text">{subtitle}</p>
+        </div>
+        <div className="flex items-center gap-1">
+          <IoStar className="text-[#e1c03f] textbase" />
+          <p className="text-sm font-inter font-medium text-text">{count}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BaseIconStats.propTypes = {
+  ICON: PropTypes.element,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  count: PropTypes.string,
+  cssClass: PropTypes.string,
+  color: PropTypes.string,
+};
+
+export default BaseIconStats;
